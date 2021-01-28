1f0715bf2c4371e7bf2c012c10a80a6eb56c601d
DESKTOP-5M61PMM\66983 <Sarapipat.samr@bu.ac.th>
2021-01-28 22:41:03 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Week 1/script/TextText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class TextText : MonoBehaviour
+{
+    public GameObject InputField;
+    public Text result;
+    public Text data;
+    public string[] textData;
+
+    void Start()
+    {
+        for (int i = 0; i < textData.Length; i++)
+        {
+            print(textData[i]);
+            data.text += textData[i] + "\n";
+        }
+    }
+
+    public void TextCheck()
+    {
+        name = InputField.GetComponent<Text>().text;
+
+        if (textData.Contains(name))
+        {
+            print(name + " is found");
+            result.text = "[ " + name + " ] is found.";
+        }
+        else
+        {
+            print(name + " is not found");
+            result.text = "[ " + name + " ] is not found.";
+        }
+    }
+}
